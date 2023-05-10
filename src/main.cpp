5034e4692a82577b7270192e269497359181cf96
braincurd <123986726+braincurd@users.noreply.github.com>
2023-05-10 10:47:47 +0200

    0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 #include <ota.h>
 
 // This string should correspond to github tag used for Releasing (via. Github Actions)
-#define VERSION "0.0.2"
+#define VERSION "0.0.1"
 
 // Replace your_username/your_repo with your values (ex. axcap/Esp-GitHub-OTA)
 // This is a link to repo where your firmware updates will be pulled from
-// #define RELEASE_URL "https://api.github.com/repos/your_username/your_repo/releases/latest"
+// #define RELEASE_URL "https://api.github.com/repos/braincurd/githubtest/releases/latest"
 
 // Use this version of the URL together with init_ota(VERSION, true) under debugging
 // to spare yourself from getting timeout from GitHub API
-#define RELEASE_URL "https://github.com/braincurd/githubtest.git"
+ #define RELEASE_URL "https://api.github.com/repos/braincurd/githubtest/releases/latest"
 
-#define DELAY_MS 20
+#define DELAY_MS 1000
 
 #define SSID "aLANdin"
 #define PASSWORD "50737simplenetz"
